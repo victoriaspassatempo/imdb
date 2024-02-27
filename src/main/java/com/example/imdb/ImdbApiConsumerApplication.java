a47047cd5a36417657cc9df35eea12e9e1158554
victoria <victoriaspassatempo@gmail.com>
2024-02-27 20:54:34 -0300

    iniciando projeto

---
+++ src/main/java/com/example/imdb/ImdbApiConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.example.imdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImdbApiConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImdbApiConsumerApplication.class, args);
+	}
+
+}
